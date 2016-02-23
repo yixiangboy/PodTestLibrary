903f675070c3f428a9d7b6923c886b6405bf7370
yixiang <yixiang.yx@alibaba-inc.com>
2016-02-23 11:56:50 +0800

    添加方法

---
+++ Pod/Classes/YXPeople.h
@@ -0,0 +1,15 @@
+//
+//  YXPeople.h
+//  Pods
+//
+//  Created by yixiang on 16/2/23.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YXPeople : NSObject
+
++(void)sayHello;
+
+@end
---
+++ Pod/Classes/YXPeople.m
@@ -0,0 +1,17 @@
+//
+//  YXPeople.m
+//  Pods
+//
+//  Created by yixiang on 16/2/23.
+//
+//
+
+#import "YXPeople.h"
+
+@implementation YXPeople
+
++(void)sayHello{
+    NSLog(@"HELLO COCOAPODS");
+}
+
+@end
