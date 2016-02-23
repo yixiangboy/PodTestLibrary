e7d636dc4b5254fcff3329060c3c4c1979a2634c
yixiang <yixiang.yx@alibaba-inc.com>
2016-02-23 12:00:43 +0800

    pod update

---
+++ Example/Pods/Headers/Private/podTestLibrary/YXPeople.h
@@ -0,0 +1,1 @@
+../../../../../Pod/Classes/YXPeople.h
--- Example/Pods/Target Support Files/podTestLibrary/podTestLibrary-umbrella.h
+++ Example/Pods/Target Support Files/podTestLibrary/podTestLibrary-umbrella.h
@@ -1,5 +1,6 @@
 #import <UIKit/UIKit.h>
 
+#import "YXPeople.h"
 
 FOUNDATION_EXPORT double podTestLibraryVersionNumber;
 FOUNDATION_EXPORT const unsigned char podTestLibraryVersionString[];
